9a5f03b8300c3887926db26d03dff2992437fdd6
Danil Merinov <danilmerinov9@gmail.com>
2024-09-28 15:42:50 -0400

    added graceful shutdown with defer func

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/dan1lm/go-microservice.git/application"
 )
 
 func main(){
-	app:= application.New()
-	err:= app.Start(context.TODO())
+	app := application.New()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	
+	defer cancel()
+
+	err:= app.Start(ctx)
+
 	if err!=nil{
 		fmt.Println("failed to start app: ", err)
 	}
